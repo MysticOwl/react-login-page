0e1078cf05f9d84340240eb86c3ca31bd5a855c2
Thiago Copello <thiaago.copello@gmail.com>
2022-07-28 09:51:47 -0300

    Login Page v1.1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
 
-        <LoginPage/>
+        <LoginPage className="loginArea" labelLogin="Usuário" labelPassword="Senha"/>
 
     </div>
   );
